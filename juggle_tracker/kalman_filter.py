fc76cf45e1f5ebf2adf3ce6c3d36e5f1e02c39f9
Marc Rasi <marcrasi@google.com>
2020-12-22 21:36:05 -0800

    add kalman observation method

--- juggle_tracker/kalman_filter.py
+++ juggle_tracker/kalman_filter.py
@@ -43,7 +43,9 @@ def transition_matrix(dt: float):
                   [0, 0,            1]])
     return scipy.linalg.block_diag(a, a)
 
-def kalman_transition(states: KalmanStates, dt: float, hp: KalmanHyperparameters):
+def kalman_transition(
+        states: KalmanStates, dt: float, hp: KalmanHyperparameters):
+    """Returns `states`, stepped forward `dt` in time."""
     t = transition_matrix(dt)[np.newaxis, ...]
     tt = np.transpose(t, axes=(0, 2, 1))
     # TODO: Multiply sds by sqrt(dt)??
@@ -63,5 +65,53 @@ def kalman_transition(states: KalmanStates, dt: float, hp: KalmanHyperparameters
     ])[np.newaxis, ...]
     return KalmanStates(
         means = (t @ states.means[..., np.newaxis]).squeeze(-1),
-        covariances = t @ states.covariances @ tt + process_noise_cov,
-    )
+        covariances = t @ states.covariances @ tt + process_noise_cov)
+
+def kalman_posterior(
+        states: KalmanStates, have_observation: np.ndarray,
+        observations: np.ndarray, hp: KalmanHyperparameters):
+    """Returns the updated state distribution given the `observations`,
+    and returns the log liklihood of `observations` under the distribution
+    given by `states` and by the observation noise model.
+
+    Arguments:
+    have_observation - [ball_count] boolean array saying whether we
+      have an observation for each ball.
+    observations - [sum(have_observation), 2] array of (x, y) observations.
+    """
+
+    if np.sum(have_observation) == 0:
+        return states, 0.0
+
+    observation_matrix = np.array([
+        [1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0],
+    ])[np.newaxis, ...]
+    observation_matrix_T = np.transpose(observation_matrix, axes=(0, 2, 1))
+    obs_noise_cov = \
+        np.diag([hp.observation_sd**2, hp.observation_sd**2])[np.newaxis, ...]
+
+    masked_means = states.means[have_observation, ..., np.newaxis]
+    masked_covs = states.covariances[have_observation, ...]
+
+    residual = \
+        observations[..., np.newaxis] - (observation_matrix @ masked_means)
+    residual_T = np.transpose(residual, axes=(0, 2, 1))
+    S = observation_matrix @ masked_covs @ observation_matrix_T + obs_noise_cov
+    S_inv = np.linalg.inv(S)
+    K = masked_covs @ observation_matrix_T @ S_inv
+
+    n_obs = 2 * np.sum(have_observation).item()
+    a = -0.5 * (residual_T @ S_inv @ residual).sum().item()
+    b = -0.5 * n_obs * np.log(2 * math.pi).item()
+    c = -0.5 * np.log(np.linalg.det(S)).sum().item()
+    obs_logp = a + b + c
+
+    new_means = states.means.copy()
+    new_covs = states.covariances.copy()
+    new_means[have_observation, ...] = \
+        (masked_means + (K @ residual)).squeeze(-1)
+    new_covs[have_observation, ...] = \
+        (np.eye(6) - K @ observation_matrix) @ masked_covs
+
+    return KalmanStates(means=new_means, covariances=new_covs), obs_logp
--- juggle_tracker/tests/test_kalman_filter.py
+++ juggle_tracker/tests/test_kalman_filter.py
@@ -12,10 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import math
 import numpy as np
 import scipy.linalg
 
 import juggle_tracker.kalman_filter as kf
+
+def normal_log_pdf(x, mean, variance):
+    return -0.5 * np.log(2 * math.pi * variance) + \
+            -0.5 * ((x - mean) ** 2) / variance 
 
 def test_kalman_transition():
     hp = kf.KalmanHyperparameters(
@@ -52,3 +57,125 @@ def test_kalman_transition():
         scipy.linalg.block_diag(expected_cov_block, expected_cov_block),
     ])
     np.testing.assert_allclose(new_states.covariances, expected_covariances)
+
+def test_kalman_posterior_at_mean():
+    """One ball, one observation exactly at the mean of the state."""
+    hp = kf.KalmanHyperparameters(
+        transition_pos_sd = 1.,
+        transition_v_sd = 2.,
+        transition_a_sd = 3.,
+        observation_sd = 2.,
+    )
+    states = kf.KalmanStates(
+        means = np.array([
+            [0., 10., 0., 0., 20., 10.],
+        ]),
+        covariances = np.array([
+            np.diag([1., 1., 1., 1., 1., 1.]),
+        ])
+    )
+
+    new_states, observation_logp = kf.kalman_posterior(
+        states, np.array([True]), np.array([0., 0.]), hp)
+
+    expected_means = states.means
+    np.testing.assert_allclose(new_states.means, expected_means)
+    expected_cov_block = np.array([
+        [0.8, 0., 0.],
+        [0., 1., 0.],
+        [0., 0., 1.],
+    ])
+    expected_covariances = np.array([
+        scipy.linalg.block_diag(expected_cov_block, expected_cov_block)
+    ])
+    np.testing.assert_allclose(new_states.covariances, expected_covariances)
+
+    # The x-coordinate observation has mean 0 and variance
+    #   state_variance + observation_variance = 1 + 4 = 5.
+    # The y-coordinate observation has the same mean and variance.
+    expected_observation_logp = normal_log_pdf(0., mean=0., variance=5.) + \
+        normal_log_pdf(0., mean=0., variance=5.)
+    np.testing.assert_allclose(observation_logp, expected_observation_logp)
+
+def test_kalman_posterior_away_mean():
+    """One ball, one observation away from the mean of the state."""
+    hp = kf.KalmanHyperparameters(
+        transition_pos_sd = 1.,
+        transition_v_sd = 2.,
+        transition_a_sd = 3.,
+        observation_sd = 2.,
+    )
+    states = kf.KalmanStates(
+        means = np.array([
+            [0., 10., 0., 0., 20., 10.],
+        ]),
+        covariances = np.array([
+            np.diag([1., 1., 1., 1., 1., 1.]),
+        ])
+    )
+
+    new_states, observation_logp = kf.kalman_posterior(
+        states, np.array([True]), np.array([10., 20.]), hp)
+
+    expected_means = np.array([[2., 10., 0., 4., 20., 10.]])
+    np.testing.assert_allclose(new_states.means, expected_means)
+    expected_cov_block = np.array([
+        [0.8, 0., 0.],
+        [0., 1., 0.],
+        [0., 0., 1.],
+    ])
+    expected_covariances = np.array([
+        scipy.linalg.block_diag(expected_cov_block, expected_cov_block)
+    ])
+    np.testing.assert_allclose(new_states.covariances, expected_covariances)
+
+    # The x-coordinate observation has mean 0 and variance
+    #   state_variance + observation_variance = 1 + 4 = 5.
+    # The y-coordinate observation has the same mean and variance.
+    expected_observation_x_logp = -0.5 * np.log(2 * math.pi * 5) - 0.5 * (10. / math.sqrt(5.)) ** 2
+    expected_observation_y_logp = -0.5 * np.log(2 * math.pi * 5) - 0.5 * (20. / math.sqrt(5.)) ** 2
+    expected_observation_logp = normal_log_pdf(10., mean=0., variance=5.) + \
+        normal_log_pdf(20., mean=0., variance=5.)
+    np.testing.assert_allclose(observation_logp, expected_observation_logp)
+
+def test_kalman_posterior_mask():
+    """Two balls, but one of them is not observed."""
+    hp = kf.KalmanHyperparameters(
+        transition_pos_sd = 1.,
+        transition_v_sd = 2.,
+        transition_a_sd = 3.,
+        observation_sd = 2.,
+    )
+    states = kf.KalmanStates(
+        means = np.array([
+            [15., 10., 0., 30., 20., 10.],
+            [0., 10., 0., 0., 20., 10.],
+        ]),
+        covariances = np.array([
+            np.diag([1., 1., 1., 1., 1., 1.]),
+            np.diag([1., 1., 1., 1., 1., 1.]),
+        ])
+    )
+
+    new_states, observation_logp = kf.kalman_posterior(
+        states, np.array([False, True]), np.array([0., 0.]), hp)
+
+    expected_means = states.means
+    np.testing.assert_allclose(new_states.means, expected_means)
+    expected_cov_block = np.array([
+        [0.8, 0., 0.],
+        [0., 1., 0.],
+        [0., 0., 1.],
+    ])
+    expected_covariances = np.array([
+        np.diag([1., 1., 1., 1., 1., 1.]),
+        scipy.linalg.block_diag(expected_cov_block, expected_cov_block),
+    ])
+    np.testing.assert_allclose(new_states.covariances, expected_covariances)
+
+    # The x-coordinate observation has mean 0 and variance
+    #   state_variance + observation_variance = 1 + 4 = 5.
+    # The y-coordinate observation has the same mean and variance.
+    expected_observation_logp = normal_log_pdf(0., mean=0., variance=5.) + \
+        normal_log_pdf(0., mean=0., variance=5.)
+    np.testing.assert_allclose(observation_logp, expected_observation_logp)
